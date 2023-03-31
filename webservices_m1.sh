c2ee26f33d373f9b8c47e5aeb7e9c0099cd4b3fa
Ben Devries <benjamin.devries@002PRD-M-BDevr.local>
2023-03-31 10:44:30 -0600

    added fix for security 16 image

--- webservices_m1.sh
+++ webservices_m1.sh
@@ -2,6 +2,7 @@
 
 echo Setting up webservices...
 
+# Update Dockerfiles to point to openjdk:17
 for d in */
 do
     if test -f "$d"Dockerfile
@@ -13,6 +14,7 @@ do
     fi
 done
 
+# Update docker-compose.yml to use linux x86 platform
 if ! grep -q 'platform: linux/x86_64' 'docker-compose.yml'; then
     sed -i '' '/connection-manager-db:/a\
     platform: linux/x86_64
@@ -23,6 +25,13 @@ if ! grep -q 'platform: linux/x86_64' 'docker-compose.yml'; then
     echo Updating docker-compose.yml for correct MySQL platform...;
 fi
 
+# Update docker-compose.yml security16 image
+if grep -q '489561981168.dkr.ecr.us-east-1.amazonaws.com/security_16_test:2.7' 'docker-compose.yml'; then
+    echo Updating docker-compose.yml for correct security 16 image...
+fi
+sed -i '' 's/489561981168.dkr.ecr.us-east-1.amazonaws.com\/security_16_test:2.7/489561981168.dkr.ecr.us-east-1.amazonaws.com\/security_16_arm:1.2/g' 'docker-compose.yml'
+
+# Add flyway.env file if missing
 if ! test -f "flyway.env"; then
     echo; read -p 'Enter SPRING_FLYWAY_LICENSE_KEY: ' key; echo
     echo "SPRING_FLYWAY_LICENSE_KEY=$key" > flyway.env
